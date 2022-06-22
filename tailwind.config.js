8b2de38fd9d62490e2ec9f4017d9cea2fdd4162a
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-06-22 14:00:11 +0700

    color config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      primary: '#1363DF',
+      secondary: '#47B5FF',
+      light: '#DFF6FF',
+      dark: '#06283D',
+    },
     extend: {},
   },
   plugins: [],
