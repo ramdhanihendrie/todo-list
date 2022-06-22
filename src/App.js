16c41b2b1a4bbd4c45f78254c091da2c164c8907
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-06-22 18:01:56 +0700

    create header, input, and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="text-center flex flex-col h-screen">
+      <header className="bg-dark py-5 fixed z-50 w-full">
+        <a href="" className="text-3xl font-bold text-light">Todo List App</a>
       </header>
+
+      <main className="bg-light text-dark flex-grow pt-32">
+        <form>
+          <input 
+            type="text" 
+            name="todo" 
+            placeholder="Todo..." 
+            className="border-2 border-primary rounded-md py-3 px-2 mr-5 w-1/2"
+          />
+          <input 
+            type="submit" 
+            value="Submit"
+            className="bg-primary text-light py-3 px-5 rounded-md cursor-pointer"
+          />
+        </form>
+      </main>
+      
+
+      <footer className="bg-dark text-light py-2">
+        <p>Made with ♥ by Hendrie Ramdhani</p>
+      </footer>
     </div>
   );
 }
