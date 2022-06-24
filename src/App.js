4467b01816377d71d99d72addc3ad3d668e44f11
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-06-24 22:09:01 +0700

    add handle submit, completed, and delete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-function App() {
+import React, {useState} from 'react';
+import CardTodo from './components/cardTodo';
+
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  
+  const handleSubmit = (e) => {
+    if (todo === '') {
+      //alert("Insert a task!");
+      e.preventDefault();
+      return false;
+    }
+
+    const newTodo = { id: Math.floor(Math.random() * 100), todo: todo, isCompleted: false }; 
+    setTodos([...todos, newTodo]);
+    setTodo("");
+    
+    e.preventDefault();
+    e.target.reset();
+  }
+  
+  
   return (
-    <div className="text-center flex flex-col h-screen">
+    <div className="text-center flex flex-col min-h-screen">
       <header className="bg-dark py-5 fixed z-50 w-full">
-        <a href="" className="text-3xl font-bold text-light">Todo List App</a>
+        <a href="/" className="text-3xl font-bold text-light">Todo List App</a>
       </header>
 
-      <main className="bg-light text-dark flex-grow pt-32">
-        <form>
+      <main className="bg-light text-dark flex-grow pt-32 pb-5">
+        <form onSubmit={handleSubmit}>
           <input 
             type="text" 
-            name="todo" 
+            onChange={(e) => {setTodo(e.target.value)}}
             placeholder="Todo..." 
             className="border-2 border-primary rounded-md py-3 px-2 mr-5 w-1/2"
-          />
+            />
           <input 
             type="submit" 
             value="Submit"
             className="bg-primary text-light py-3 px-5 rounded-md cursor-pointer"
           />
         </form>
+        
+        <CardTodo todos={todos} setTodos={setTodos} />
       </main>
       
 
---
+++ src/components/cardTodo.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Icon } from '@iconify/react';
+
+const cardTodo = ({todos, setTodos}) => {
+  const handleCompleted = (todo) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, isCompleted: !item.isCompleted };
+        }
+        return item;
+      }),
+    );
+  };
+
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <section id="todoList" className="mt-10">
+      <h2 className="text-xl font-semibold text-dark">Todo</h2>
+      {
+        todos.map((todo) => {
+          return (
+            <div className="flex flex-col text-left px-5" key={todo.id}>
+              <div className="bg-primary text-white rounded-lg p-6 shadow-xl w-full max-w-3xl mx-auto my-2 flex justify-between gap-10">
+                <h5 className={(todo.isCompleted) ? "line-through" : ""}>{(todo.todo) && todo.todo}</h5>
+                <div className="button-container flex gap-2">
+                  <Icon 
+                    icon="akar-icons:check-box" 
+                    width="24" 
+                    height="24" 
+                    className={(todo.isCompleted) ? "hidden" : "hover:text-green-500 cursor-pointer"} 
+                    onClick={() => {handleCompleted(todo)}}
+                  />
+                  <Icon 
+                    icon="akar-icons:edit" 
+                    width="24" 
+                    height="24" 
+                    className={(todo.isCompleted) ? "hidden" : "hover:text-yellow-400 cursor-pointer"}
+                  />
+                  <Icon 
+                    icon="akar-icons:trash-can" 
+                    width="24" 
+                    height="24" 
+                    className='hover:text-red-600 cursor-pointer' 
+                    onClick={() => {handleDelete(todo)}}
+                  />
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+    </section>
+  )
+}
+
+export default cardTodo
